1f6d4538281f2f1e2d87012734e9e5cf54cfa062
codrin2 <hssang127@gmail.com>
2023-12-10 19:58:03 +0900

    build: eureka clinet 추가

--- src/main/java/com/poppin/likeservice/LikeServiceApplication.java
+++ src/main/java/com/poppin/likeservice/LikeServiceApplication.java
@@ -2,10 +2,12 @@ package com.poppin.likeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
 @EnableScheduling
+@EnableDiscoveryClient
+@SpringBootApplication
 public class LikeServiceApplication {
 
 	public static void main(String[] args) {
