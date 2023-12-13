454a5e1efb991d28f0e04b6b3d0b73e869fe57a1
Yany Choi <dydghks0818@naver.com>
2023-12-13 14:29:12 +0900

    update env

--- src/main/java/com/poppin/likeservice/LikeServiceApplication.java
+++ src/main/java/com/poppin/likeservice/LikeServiceApplication.java
@@ -2,11 +2,9 @@ package com.poppin.likeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@EnableDiscoveryClient
 @SpringBootApplication
 public class LikeServiceApplication {
 
